cf5f287c6e91df1af1a4fc072d6d9df8fd96e0c2
Filip Cederquist <cederquist94@hotmail.com>
2024-03-27 08:13:56 +0100

    Update to working flow with model switch

--- downloads/downloader.py
+++ downloads/downloader.py
@@ -2,8 +2,9 @@ import os
 from pytube import YouTube
 from pytube.exceptions import PytubeError
 
+
 class Downloader:
-    def __init__(self, download_path='downloaded_audio'):
+    def __init__(self, download_path="downloaded_audio"):
         self.download_path = download_path
         os.makedirs(self.download_path, exist_ok=True)
 
@@ -15,11 +16,15 @@ class Downloader:
                 return None
 
             audio_file_path = os.path.join(self.download_path, f"{video.video_id}.mp4")
-            audio_streams.download(output_path=self.download_path, filename=f"{video.video_id}.mp4")
+            audio_streams.download(
+                output_path=self.download_path, filename=f"{video.video_id}.mp4"
+            )
             return audio_file_path
         except PytubeError as e:
             print(f"An error occurred while downloading the video {video.title}: {e}")
             return None
         except Exception as e:
-            print(f"An unexpected error occurred while processing the video {video.title}: {e}")
+            print(
+                f"An unexpected error occurred while processing the video {video.title}: {e}"
+            )
             return None
